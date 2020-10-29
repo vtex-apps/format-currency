246a72c17d2d322122fbd83e12ffa2c9419503b1
Breno Calazans <brenocalazans@gmail.com>
2020-10-29 16:00:49 -0300

    Make condition more explicit

--- react/modules/numberRound.ts
+++ react/modules/numberRound.ts
@@ -1,7 +1,11 @@
 export function roundUp(number: number, decimals = 2): number {
   const [, decimalPlaces] = number.toString().split('.')
 
-  if (decimals > 0 && decimalPlaces && decimalPlaces.length > decimals) {
+  if (
+    decimals > 0 &&
+    decimalPlaces != null &&
+    decimalPlaces.length > decimals
+  ) {
     const rounding = 1 / 10 ** decimals
 
     return parseFloat((number + rounding).toFixed(decimals))
